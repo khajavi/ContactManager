cde67acc546ea4752dd59f1d2e2bfa817a51cbd9
BBK-PiJ-2012-88 <edhogarth@hotmail.com>
2013-01-12 15:22:03 +0000

    Added the getFutureMeeting method. It's implemented as the inverse if the getPastMeeting method

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -27,9 +27,11 @@ public class ContactManagerImpl implements ContactManager {
 		}
 		
 		/**
-		 * Checks whether or not a meeting with this ID is scheduled for the future.
-		 * If scheduled for the future an IllegalArgumentException is thrown, else the meeting 
-		 * with that ID (possibly none) is returned.
+		 * The method checks whether or not a meeting with this ID has been scheduled for the future,
+		 * by iterating through the list of FutureMeetings. If scheduled for the future an 
+		 * IllegalArgumentException is thrown. If no meeting is scheduled in the future with this id
+		 * the method iterates through the list of PastMettings checking whether or not a meeting with
+		 * this id exists. If found the PastMeeting with this id is returned, else null is returned.
 		 */
 		
 		public PastMeeting getPastMeeting(int id){
@@ -41,6 +43,24 @@ public class ContactManagerImpl implements ContactManager {
 			for(int i = 0; i < PastMeetings.size(); i++){
 				if(PastMeetings.get(i).getID() == id){
 					return PastMeetings.get(i);
+				}
+			}
+			return null;
+		}
+		
+		/**
+		 * Identical to the method getPastMeeting, except FutureMeeting
+		 * and PastMeeting are inverted.
+		 */
+		public FutureMeeting getFutureMeeting(int id){
+			for (int i = 0; i < PastMeetings.size(); i++){
+				if(PastMeeting.get(i).getID() == id){
+					throw new IllegalArgumentException("This meeting has already taken place");
+				}
+			}
+			for (int i = 0; i < FutureMeetings.size(); i++){
+				if(FutureMeetings.get(i).getID() == id){
+					return FutureMeetings.get(i);
 				}
 			}
 			return null;
