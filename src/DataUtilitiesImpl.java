2f9599ad18e5027d290768c484ffed7d9c0e635f
BBK-PiJ-2012-88 <edhogarth@hotmail.com>
2013-01-27 18:28:20 +0000

    Finished all the loading data methods in data utilities. Haven't tested them yet.

--- src/DataUtilitiesImpl.java
+++ src/DataUtilitiesImpl.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.LinkedHashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -67,9 +69,9 @@ public class DataUtilitiesImpl implements DataUtilities {
 		return name;
 	}
 	
-	public int getID(String data){
+	public int getContactID(String data){
 		
-		Pattern getId = Pattern.compile("Id:\\d*");
+		Pattern getId = Pattern.compile("Contact Id:\\d*");
 		Pattern findId = Pattern.compile("[0-9]+");
 		int id = 0;
 		Matcher m = getId.matcher(data);
@@ -98,17 +100,96 @@ public class DataUtilitiesImpl implements DataUtilities {
 	public void loadContact(String data){
 		
 		String name = getContactName(data);
-		int id = getID(data);
+		int id = getContactID(data);
 		String notes = getNotes(data);
 		Contacts.add(new ContactImpl(name, notes, id));	
 	}
 	
+	public int getMeetingID(String data){
+		
+		Pattern getId = Pattern.compile("Meeting Id:\\d*");
+		Pattern findId = Pattern.compile("[0-9]+");
+		int id = 0;
+		Matcher m = getId.matcher(data);
+		while(m.find()){
+			
+			Matcher m2 = findId.matcher(m.group());
+			while(m2.find()){
+				id = Integer.parseInt(m2.group());
+			}
+		}
+		return id;
+	}
+	
+	public LinkedHashSet<Contact> getAttendeeIds(String data){
+		
+		LinkedHashSet<Contact> Ids = new LinkedHashSet<Contact>();
+		Pattern ContactIds = Pattern.compile("Contact Id:(([0-9]*,)*)");
+		Pattern findId = Pattern.compile("[0-9]+");
+		ArrayList<Integer> listIds = new ArrayList<Integer>();
+		
+		Matcher m = ContactIds.matcher(data);
+		while(m.find()){
+			Matcher m2 = findId.matcher(m.group());
+			while(m2.find()){
+				int id = Integer.parseInt(m2.group());
+				listIds.add(id);
+			}
+		}
+		for(Integer id: listIds){
+			for (Contact c: Contacts){
+				if(c.getId() == id){
+					Ids.add(c);
+				}
+			}
+		}
+		return Ids;
+	}
+	
+	public GregorianCalendar getMeetingDate(String data){
+		
+		Pattern getDate = Pattern.compile("(Date:([0-9]{4})/([0-9]{2})/([0-9]{2}))");
+		Matcher m = getDate.matcher(data);
+		String date = "";
+		while(m.find()){
+			date = m.group();
+		}
+		int year = Integer.parseInt(date.substring(5,9));
+		date = date.substring(10);
+		int month = Integer.parseInt(date.substring(0,2));
+		date = date.substring(3);
+		int day = Integer.parseInt(date.substring(0, 2));
+		date = date.substring(3);
+		Calendar meeting = new GregorianCalendar(year, month, day);
+		return (GregorianCalendar) meeting;
+	}
+	
 	public void loadMeeting(String data){
 		
+		int id = getMeetingID(data);
+		LinkedHashSet<Contact> Ids = getAttendeeIds(data);
+		GregorianCalendar meetingDate = getMeetingDate(data);
+		
+		Calendar now = new GregorianCalendar();
+		if(meetingDate.after(now)){
+			FutureMeeting futureMeeting = new FutureMeetingImpl(Ids, meetingDate, id);
+			FutureMeetings.add(futureMeeting);
+		}else{
+			String notes = "";
+			PastMeeting pastMeeting = new PastMeetingImpl(meetingDate, Ids, notes, id);
+			PastMeetings.add(pastMeeting);
+		}		
 	}
 	
 	public void loadPastMeeting(String data){
 		
+		int id = getMeetingID(data);
+		String notes = getNotes(data);
+		GregorianCalendar meetingDate = getMeetingDate(data);
+		LinkedHashSet<Contact> attendees = getAttendeeIds(data);
+		
+		PastMeeting pastMeeting = new PastMeetingImpl(meetingDate, attendees, notes, id);
+		PastMeetings.add(pastMeeting);
 	}
 
 	
